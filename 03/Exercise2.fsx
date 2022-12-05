98d64715ae5033f95ff232f566a5c9698b94d7bf
Matthew Herman <mattherman11@gmail.com>
2022-12-04 22:36:09 -0600

    Day 3 cleanup

--- 03/Exercise2.fsx
+++ 03/Exercise2.fsx
@@ -3,8 +3,7 @@
 open Common
 open System
 
-let parseRucksack (rucksack: string) =
-    Set.ofSeq rucksack
+let parseRucksack = Set.ofSeq
 
 let findBadge elfGroup =
     Seq.reduce (Set.intersect) elfGroup |> Seq.head
@@ -16,10 +15,11 @@ let itemPriority (itemChar: char) =
         (int itemChar) - 96
 
 let solve file =
-    let input = readInput file
-    let rucksacks = Seq.map parseRucksack input
-    let elfGroups = Seq.chunkBySize 3 rucksacks
-    elfGroups |> Seq.map (findBadge >> itemPriority) |> Seq.sum
+    readInput file
+    |> Seq.map parseRucksack
+    |> Seq.chunkBySize 3
+    |> Seq.map (findBadge >> itemPriority)
+    |> Seq.sum
 
 let solution = solve "input.txt"
 printfn "%d" solution
